5cf785defaeb17e57f5e020ec9367417b7bf1e64
叉叉恩 <chachaen@JXs-MacBook-Air.local>
2023-02-06 15:41:16 -0500

    Jaclyn

---
+++ main.js
@@ -0,0 +1,88 @@
+
+// JS File for class demo 
+// Ab Mosca 
+// Last moditifed: 01.30.2023 
+
+
+// print
+
+console.log("hello world");
+
+
+// variables
+
+// constants
+const myCourse = "DS4200";
+console.log(myCourse);
+
+
+// myCourse = 'DS2000';
+
+let season = "Winter";
+console.log(season);
+
+season = "Spring";
+console.log(season);
+
+
+// var
+var x = 7;
+
+// type
+console.log(typeof(season));
+
+let hwDue = true;
+console.log(typeof(hwDue));
+
+let faveNum = 6;
+console.log(typeof(faveNum));
+
+
+// loosely tyoes language
+faveNum = "six";
+console.log(typeof(faveNum));
+
+
+
+// functions
+function tenTimes(num) {
+	let result = num *10;
+	return result;
+}
+
+
+let ans = tenTimes(6);
+console.log(ans);
+
+
+// button function
+function buttonClicked() {
+	console.log("button was clicked!")
+
+	let newText = "Button was clikced";
+
+	// select the buttom which modify
+	let  buttonDiv = document.getElementById("button-div");
+
+	buttonDiv.innerHTML = newText;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
