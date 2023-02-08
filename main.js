b7602a3a0518ee27b6286e384a7c6b5b49b3944a
叉叉恩 <chachaen@JXs-MacBook-Air.local>
2023-02-08 10:10:58 -0500

    final version

--- main.js
+++ main.js
@@ -2,12 +2,14 @@
 // button function
 
 
+// begining number as 1
 var num = 1;
 
 function buttonClicked() {
 
 	console.log("button was clicked!")
 
+	// which result will show when clicked the botton
 	let newText = "Number of times button clicked:"+ (num++);
 
 	// select the buttom which modify
